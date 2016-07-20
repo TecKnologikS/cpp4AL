1d7615a88c7ba736b7eed37b5c8df4295f151a48
TecKnologikS <robinpauquet@gmail.com>
2016-07-20 21:39:41 +0000

    work

--- gpio.cpp
+++ gpio.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
 #include <errno.h>
@@ -11,11 +12,11 @@ int main(int argc, char* argv[]) {
 
     if (argc > 1) {
         GPIOClass* gpio = new GPIOClass(argv[0]); 
-        if(strncmp(argv[1], "enable", 0) {
+        if(strncmp(argv[1], "enable", 0)) {
             gpio->setval_gpio("1");
-        } else if(strncmp(argv[1], "disable", 0) {
+        } else if(strncmp(argv[1], "disable", 0)) {
                 gpio->setval_gpio("0");
-        } else if(strncmp(argv[1], "change", 0) {
+        } else if(strncmp(argv[1], "change", 0)) {
             string inputstate;
             gpio->getval_gpio(inputstate); 
             if(inputstate == "0")
