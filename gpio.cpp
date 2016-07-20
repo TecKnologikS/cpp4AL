f2e5bb0625db48b777ca0aa1d1cfe151ec3fbd2d
= <robinpauquet@gmail.com>
2016-07-20 23:48:55 +0200

    l

--- gpio.cpp
+++ gpio.cpp
@@ -10,14 +10,14 @@ using namespace std;
 
 int main(int argc, char* argv[]) { 
     std::cout << "nb param " << argc  << std::endl;
-    if (argc > 1) {
-        GPIOClass* gpio = new GPIOClass(argv[0]); 
-        cout << " gpio " << argv[0] << " command " << argv[1] << endl;
-        if(strncmp(argv[1], "enable", 0)) {
+    if (argc > 2) {
+        GPIOClass* gpio = new GPIOClass(argv[1]); 
+        cout << " gpio " << argv[1] << " command " << argv[2] << endl;
+        if(strncmp(argv[2], "enable", 0)) {
             gpio->setval_gpio("1");
-        } else if(strncmp(argv[1], "disable", 0)) {
+        } else if(strncmp(argv[2], "disable", 0)) {
                 gpio->setval_gpio("0");
-        } else if(strncmp(argv[1], "change", 0)) {
+        } else if(strncmp(argv[2], "change", 0)) {
             string inputstate;
             gpio->getval_gpio(inputstate); 
             if(inputstate == "0")
